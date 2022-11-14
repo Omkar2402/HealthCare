fb31d2213c746f55923b5ac62c28e509d0e409a1
Omkar Khare <khareomkar24@gmail.com>
2022-11-14 15:28:03 +0530

    Added add user and ratereview

---
+++ src/HealthCare.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class HealthCare {
+
+    void addUser(String name, String email) {
+        if (name.length()>0 && email.length()>0) {
+            System.out.println(name+" your registration was successful");
+        }
+        else if (name.length()<=0) {
+            System.out.println("Invalid name");
+        }
+        else if (email.length()<=0) {
+            System.out.println("Invalid email");
+        }
+    }
+
+    void ratereviewrecipe(String username, String recipe, double rate, String review) {
+        System.out.println(username+" your rating and review was successfully saved");
+        System.out.println("Recipe name: "+recipe);
+        System.out.println("Your rating: "+rate);
+        System.out.println("Your review: "+review);
+    }
+
+    
+
+
+    public static void main(String[] args) {
+        HealthCare obj = new HealthCare();
+        obj.addUser("Abc", "as");
+        obj.ratereviewrecipe("Abc","Salad",4.3,"Very good recipe.");
+
+
+    }
+
+}
